ae69009205d181b94c1b5fa7a1cc0a48468fa6c7
Dwarapu Ravi Teja <ravitejadwarapu9@gmail.com>
2026-06-22 01:30:17 +0530

    Clean up geminiService.js by removing logs
    Removed console logs for sensitive information.

--- server/services/geminiService.js
+++ server/services/geminiService.js
@@ -1,11 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { GoogleGenerativeAI } from "@google/generative-ai";
-console.log("USING GEMINI SERVICE FILE");
-console.log(
-  "Loaded Key:",
-  process.env.GEMINI_API_KEY
-);
+
 const genAI = new GoogleGenerativeAI(
   process.env.GEMINI_API_KEY
 );
