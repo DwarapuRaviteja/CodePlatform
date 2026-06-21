947e498f44bef57b5910f11da4d165a796042193
Dwarapu Ravi Teja <ravitejadwarapu9@gmail.com>
2026-06-22 01:59:40 +0530

    Update FloatingBackground.jsx

--- client/src/components/three/FloatingBackground.jsx
+++ client/src/components/three/FloatingBackground.jsx
@@ -1,5 +1,4 @@
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Float, OrbitControls } from "@react-three/drei";
+
 import { useRef } from "react";
 
 function FloatingCube({ position, color, size }) {
